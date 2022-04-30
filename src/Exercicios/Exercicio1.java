4a90e12f5f183ac43971a89bea78ff4101b2305a
nadiabsouza <nadialuna.souza@gmail.com>
2022-04-29 22:11:48 -0300

    add novas pastas

---
+++ src/Exercicios/Exercicio1.java
@@ -0,0 +1,144 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		//System.out.println("Alo mundo"); 
+		
+		// exercicio 2
+		
+		//System.out.println("Informe um numero");
+		//Scanner scan = new Scanner (System.in);
+		//int numero = scan.nextInt();
+		//System.out.println("O numero informado foi: "+numero);
+;
+		// exercicio 3
+       
+//       System.out.println("Informe dois numeros: ");
+//       Scanner scan = new Scanner (System.in);
+//       int numero1= scan.nextInt();
+//       int numero2= scan.nextInt();
+//       int numero3= numero1+numero2;
+//       
+//       System.out.println("Total soma: "+(numero3));
+       
+       // exercicio 4
+       
+//       System.out.println("Informe 4 notas bimestrais");
+//       Scanner scan = new Scanner (System.in);
+//       float nota1 = scan.nextFloat();
+//       float nota2 = scan.nextFloat();
+//       float nota3 = scan.nextFloat();
+//       float nota4 = scan.nextFloat();
+//       float media = (nota1+nota2+nota3+nota4)/4;
+//       
+//       System.out.println("Média das notas: "+ media);
+       
+       // exercicio 5
+		
+		//       int metros = 50;
+		//       int centimetros;
+		//       int resultado = (metros*100);
+		//       
+		//       System.out.println("Metros para centimetros: "+resultado);
+       
+    // exercicio 6
+	//		System.out.println("Informe o raio:");
+	//		Scanner scan = new Scanner (System.in);
+	//		float raio = scan.nextFloat();
+	//		float area = 3.14f * raio * raio;
+	//		System.out.println("Sua area é: "+ area);
+
+	// exercicio 7
+	//	System.out.println("Informe o lado:");
+	//	Scanner scan = new Scanner (System.in);
+	//	float lado = scan.nextFloat();
+	//	float area = lado * lado;
+	//	float dobroArea = area * 2;
+	//
+	//	System.out.println("Dobro da area é: "+ dobroArea);
+
+    //execicio 8
+
+//    System.out.println("Informe quanto você ganha por hora: ");
+//    Scanner scan = new Scanner (System.in);
+//    int numero = scan.nextInt();
+//    
+//    System.out.println("Informe quantas horas trabalhadas: ");
+//    Scanner scan1 = new Scanner (System.in);
+//    int numero2 = scan1.nextInt();
+//    
+//    int resultado= (numero*numero2)*30;
+//    
+//    System.out.println("Seu salario do mês é: "+resultado);
+    
+     //execicio 9
+    
+//     System.out.println("Informe a temperatura em graus Farenheit: ");
+//     Scanner scan = new Scanner (System.in);
+//     int temperatura = scan.nextInt();
+//     int formula = 32;
+//     float formula2=  1.8f;
+//     double conversao = (temperatura-formula)/1.8;
+//     
+//     
+//     System.out.println("Temperatura em graus Celsius é:  "+conversao);
+     
+     // exercicio 10
+     
+//     System.out.println("Informe a temperatura em graus Celsius: ");
+//     Scanner scan = new Scanner (System.in);
+//     int temperatura = scan.nextInt();
+//     int formula = 32;
+//     double conversao = (temperatura*1.8)+formula;
+//     
+//     
+//     System.out.println("Temperatura em graus Farenheit é:  "+conversao);
+//     
+    		 
+     //exercicio 11
+
+//   System.out.println("Informe dois numeros inteiros: ");
+//   Scanner scan = new Scanner (System.in);
+//   int numero1 = scan.nextInt();
+//   int numero2 = scan.nextInt();
+//   
+//   System.out.println("Informe um numero real: ");
+//   int numeroReal= scan.nextInt();
+//   Scanner scan1 = new Scanner (System.in);
+//  
+   
+//   int resultado1 = (numero1*2)+(numero2/2);
+//   System.out.println("O produto do dobro do primeiro com metade do segundo: "+resultado1);
+//   System.out.println("A soma do triplo do primeiro com o terceiro: "+((numero1*3)+(numeroReal)));
+
+//exercicio 12
+
+//     System.out.println("Informe seu peso: ");
+//     Scanner scan = new Scanner(System.in);
+//     float peso= scan.nextFloat();
+//     
+//     System.out.println("Informe sua altura: ");
+//     Scanner scan1 = new Scanner(System.in);
+//     float altura= scan1.nextFloat();
+//     float pesoIdeal= (72.7f*altura)-58;
+//     
+//     System.out.println("Seu peso ideal é: "+ pesoIdeal);
+     
+     
+     
+     
+     
+
+
+
+
+       
+
+
+	}
+
+}
---
+++ src/Exercicios/Exercicios2.java
@@ -0,0 +1,90 @@
+package Exercicios;
+import java.util.Scanner;
+
+public class Exercicios2 {
+	
+	public static void main(String[] args) {
+		
+		//exercicio 1
+		
+//		 System.out.println("Informe dois números: ");
+//	     Scanner scan = new Scanner(System.in);
+//	     int numero1 = scan.nextInt();
+//	     int numero2 = scan.nextInt();
+//	     
+//	     if (numero1>numero2){
+//	    	 System.out.println("O Maior número é: "+numero1);
+//	     }else {
+//	    	 System.out.println("O maior número é: "+numero2);
+//	     }
+		
+	
+	//exercicio 2
+//		 System.out.println("Informe um valor: ");
+//	     Scanner scan = new Scanner(System.in);
+//	     int numeroInformado = scan.nextInt();
+//	     
+//	     if (numeroInformado>0) {
+//	     System.out.println("Valor é posito!");
+//	     } else {
+//	    	 System.out.println("Valor é negativo!");
+//	     }
+	     
+//	}
+	
+//}
+		
+//		System.out.println("Entre com uma letra F ou M ");
+//	    Scanner scan = new Scanner(System.in);
+//	    String input = scan.next();
+//	    
+//	    if (input .equalsIgnoreCase("f")){
+//	    	System.out.println("F- Feminino");
+//	    } else if (input .equalsIgnoreCase("m")) {
+//	    	System.out.println("M- Masculino");
+//	    } else {
+//	    	System.out.println("Sexo inválido");
+	    
+	    
+		System.out.println("Entre com uma letra:  ");
+	    Scanner scan = new Scanner(System.in);
+	    String letra = scan.next();
+	    
+	    if (letra .equalsIgnoreCase("a") || letra .equalsIgnoreCase("e") ||  letra .equalsIgnoreCase("i") ||  letra .equalsIgnoreCase("o") ||  letra .equalsIgnoreCase("u")) {
+	    	System.out.println("Essa letra é uma vogal!");
+	    }else { 
+	        System.out.println("Essa letra é uma consoante!");
+	        	
+	        
+	        
+                
+	        
+	        	
+	    	
+	    		
+	   
+	     
+	     
+		
+	     
+	     }
+	   
+	     
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	private static void letraDigitada(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+	
+	
+	
+	
